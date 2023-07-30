a2a9523a6271afa7ac4edd38b88629377e086eb0
Siddhant-1108 <siddhant_2015@hotmail.com>
2023-07-30 13:51:29 +0530

    message

--- app/src/main/java/com/example/bhukkad/DropCheckoutActivity.java
+++ app/src/main/java/com/example/bhukkad/DropCheckoutActivity.java
@@ -1,6 +1,15 @@
 package com.example.bhukkad;
 
 
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,6 +34,8 @@ import com.cashfree.pg.core.api.exception.CFException;
 import com.cashfree.pg.core.api.utils.CFErrorResponse;
 import com.cashfree.pg.ui.api.CFDropCheckoutPayment;
 import com.cashfree.pg.ui.api.CFPaymentComponent;
+import com.onesignal.OSNotificationOpenedResult;
+import com.onesignal.OneSignal;
 
 
 import org.json.JSONArray;
@@ -31,6 +43,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONString;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.UUID;
 
 
@@ -44,63 +61,132 @@ public class DropCheckoutActivity extends AppCompatActivity  implements CFChecko
 
     String orderID;
     String paymentSessionID = "TOKEN";
+    private AlertDialog.Builder alertDialogBuilder;
+    public String appd="b2016771-c9ef-4240-9def-d6fdf2b80843";
     String responsd;
     CFSession.Environment cfEnvironment = CFSession.Environment.SANDBOX;
 
     CompositeDisposable compositeDisposable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drop_checkout);
+
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+        String ONESIGNAL_APP_ID = "3402e9f9-581c-4695-a0d1-c9e7cc18e458";
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+        OneSignal.setNotificationOpenedHandler(
+                new OneSignal.OSNotificationOpenedHandler() {
+                    @Override
+                    public void notificationOpened(OSNotificationOpenedResult result) {
+                        String actionId = result.getAction().getActionId();
+                        String type = String.valueOf(result.getAction().getType()); // "ActionTaken" | "Opened"
+
+                        String title = result.getNotification().getTitle();
+                    }
+                });
+        alertDialogBuilder = new AlertDialog.Builder(DropCheckoutActivity.this);
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setTitle("Payment Result");
+        alertDialogBuilder.setPositiveButton("Ok", (dialog, which) -> {
+            //5star do nothing
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = "bhukkad";
+            String channelName = "bhukkad";
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
         orderID = "niifanian";
         System.out.println(orderID);
         TextView tv = (TextView) findViewById(R.id.tVV);
 
-            RequestQueue queue = Volley.newRequestQueue(this);
-            orderID = UUID.randomUUID().toString();
-            String url = String.format("https://us-central1-bhukkad-1de2e.cloudfunctions.net/widgets/token?orderId=%s&orderAmount=1.00",orderID);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        orderID = UUID.randomUUID().toString();
+        String url = String.format("https://us-central1-bhukkad-1de2e.cloudfunctions.net/widgets/token?orderId=%s&orderAmount=1.00", orderID);
 
 
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            // Display the first 500 characters of the response string.
-                            System.out.println(response);
-                            try {
-                                JSONObject jsonObject = new JSONObject(response);
-                                paymentSessionID=jsonObject.getString("payment_session_id");
-                                orderID=jsonObject.getString("order_id");
-                                tv.setText(orderID);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        System.out.println(response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            paymentSessionID = jsonObject.getString("payment_session_id");
+                            orderID = jsonObject.getString("order_id");
+                            tv.setText(orderID);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    System.out.println("didn't work");
-                }
-            });
+
+
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("didn't work");
+            }
+        });
 
 
 // Add the request to the RequestQueue.
-            queue.add(stringRequest);
+        queue.add(stringRequest);
         Button btn = (Button) findViewById(R.id.pay_btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 doDropCheckoutPayment();
+                String l = "https://sandbox.cashfree.com/pg/orders/"+orderID.toString();
+                URL url = null;
+                try {
+                    url = new URL(l);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                HttpURLConnection con = null;
+                try {
+                    con = (HttpURLConnection) url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    con.setRequestMethod("GET");
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                }
+                con.setRequestProperty("Content-Type", "application/json");
+                con.setRequestProperty("x-client-id", "309643666e8fe1d426541c9c49346903");
+                con.setRequestProperty("x-client-secret","309643666e8fe1d426541c9c49346903");
+                con.setRequestProperty("x-api-version","2022-01-01");
+                try {
+                    int status = con.getResponseCode();
+                    if(status==200) nojtify();
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+//                onPaymentVerify(orderID);
+
             }
         });
+
 
     }
 
     @Override
     public void onPaymentVerify(String orderID) {
         Log.e("onPaymentVerify", "verifyPayment triggered");
+
         // Start verifying your payment
     }
 
@@ -137,8 +223,59 @@ public class DropCheckoutActivity extends AppCompatActivity  implements CFChecko
                     .build();
             CFPaymentGatewayService gatewayService = CFPaymentGatewayService.getInstance();
             gatewayService.doPayment(DropCheckoutActivity.this, cfDropCheckoutPayment);
+
         } catch (CFException exception) {
             exception.printStackTrace();
+        }
+
+    }
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, checkout.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_IMMUTABLE);
+
+        String channelId = "Bhukkad APP";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                        .setContentTitle("Order Details: ")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+    public void nojtify() throws JSONException, IOException {
+        // The notification payload
+
+        try {
+            OneSignal.postNotification(new JSONObject("{'contents': {'en':'Payment was a success'}, 'include_player_ids': ['" + OneSignal.getDeviceState().getUserId().toString() + "']}"),
+                    new OneSignal.PostNotificationResponseHandler() {
+                        @Override
+                        public void onSuccess(JSONObject response) {
+                            Log.i("OneSignalExample", "postNotification Success: " + response.toString());
+                        }
+
+                        @Override
+                        public void onFailure(JSONObject response) {
+                            Log.e("OneSignalExample", "postNotification Failure: " + response.toString());
+                        }
+                    });
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/example/bhukkad/Tag.java
+++
@@ -1,8 +0,0 @@
-package com.example.bhukkad;
-
-public class Tag {
-
-    public static final String DEBUG = "DEBUG";
-    public static final String ERROR = "ERROR";
-
-}
